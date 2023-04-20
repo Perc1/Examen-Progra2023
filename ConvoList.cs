c5c0a45effd3f37b42617a50a0a8e94738ff7bb5
RenatoPadillaSaavedra <renato.padilla.saavedra@gmail.com>
2023-04-19 23:46:06 -0500

    Renato Padilla - Creación de Lista de Conversaciónes con NPC.

---
+++ ConvoList.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen
+{
+    internal class ConvoList : NPC
+    {
+        string Convo1;
+        string Convo2;
+        string Convo3;
+
+        public ConvoList (string Convo1, string Convo2, string Convo3)
+        {
+            this.Convo1 = Convo1;
+            this.Convo2 = Convo2;
+            this.Convo3 = Convo3;
+        }
+    }
+}
